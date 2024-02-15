93827e65c504dc9bc1ef7aa6b4db4982b13c729e
YoniLabell <44677797+YoniLabell@users.noreply.github.com>
2024-02-15 12:40:20 +0200

    Update github_checks.py

--- github_checks.py
+++ github_checks.py
@@ -2,29 +2,6 @@ import argparse
 import requests
 from github import Github
 
-def get_branch_protection(owner, repo, token, branch = "main"):
-    url = f"https://api.github.com/repos/{owner}/{repo}/branches/{branch}/protection"
-    headers = {
-        "Accept": "application/vnd.github+json",
-        "Authorization": f"Bearer {token}",
-        "X-GitHub-Api-Version": "2022-11-28"
-    }
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print(f"Failed to fetch branch protection settings: {response.status_code}")
-        return None
-
-
-def list_github_actions_workflows(repo):
-    try:
-        workflows = repo.get_contents(".github/workflows")
-        print("GitHub Actions Workflows:")
-        for workflow in workflows:
-            print(workflow.path)
-    except Exception as e:
-        print(f"Error fetching GitHub Actions workflows: {e}")
 
 def list_contributors(repo):
     contributors = repo.get_contributors()
@@ -60,11 +37,9 @@ def main():
     parser.add_argument('-t', '--token', required=True, help="GitHub Personal Access Token")
     parser.add_argument('-u', '--username', required=True, help="GitHub Username")
     parser.add_argument('-r', '--repo', required=True, help="GitHub Repository Name")
-    parser.add_argument('-b', '--branch' , help="Branch Name to Check Protection Rules For")
     parser.add_argument('-c', '--contributors', action='store_true', help="List Contributors")
     parser.add_argument('-f', '--files', action='store_true', help="Check for SECURITY.md and Issue Templates")
     parser.add_argument('-s', '--settings', action='store_true', help="Check Repository Settings")
-    parser.add_argument('-a', '--actions', action='store_true', help="List GitHub Actions Workflows")
     args = parser.parse_args()
 
     if args.branch:
